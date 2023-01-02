37b66cd2409cc8043ecce75ff71885619b982f7d
elvesbd <elvesbd@yahoo.com>
2023-01-02 17:47:02 -0300

    feat: implements method create im beaches controller

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,24 @@
 import { Controller, Post } from "@overnightjs/core";
 import { Request, Response } from "express";
+import { Beach } from "@src/models/beach";
+import mongoose from "mongoose";
 
 @Controller('beaches')
 export class BeachesController {
   @Post()
   public async create(req: Request, res: Response): Promise<void> {
-    res.status(201).send(req.body)
+    try {
+      const beach = new Beach(req.body)
+      // console.log('BEACH', beach.toJSON())
+      const result = await beach.save()
+      // console.log('RESULT', result)
+      res.status(201).send(result.toJSON())
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message })
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' })
+      }
+    }
   }
 }
