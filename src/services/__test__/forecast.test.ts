1fa252af1f86d78cc1e1b50db3f6f86bf4144011
elvesbd <elvesbd@yahoo.com>
2023-01-02 17:48:59 -0300

    test: implements Beach Forecast Functional

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,6 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormGlass_normalized_3_hours.json'
-import { Beach, BeachPosition, Forecast, ForecastProcessinInternalError } from '../forecast';
+import { Forecast, ForecastProcessinInternalError } from '../forecast';
+import { Beach, BeachPosition } from '@src/models/beach';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -16,7 +17,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id'
       }
     ]
 
@@ -101,7 +101,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id'
       }
     ]
 
