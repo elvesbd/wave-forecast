c9d6fe656710ec9502ad3d5cd2ca07a114a4045c
elvesbd <elvesbd@yahoo.com>
2023-01-22 19:51:41 -0300

    feat: ensure successfully create a new user with encrypted password

---
+++ src/services/auth.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+export class AuthService {
+  public static async hashPassword(password: string, salt = 10): Promise<string> {
+    return await bcrypt.hash(password, salt)
+  }
+
+  public static async comparePasswords(password: string, hashPassword: string): Promise<boolean> {
+    return await bcrypt.compare(password, hashPassword)
+  }
+}
+
