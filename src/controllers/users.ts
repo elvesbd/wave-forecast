9b57101995158f10ebbed432858c001e9b91b36a
elvesbd <elvesbd@yahoo.com>
2023-02-04 16:29:31 -0300

    feat: add jsonwebtoken in users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from './index';
+import { AuthService } from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -9,10 +10,27 @@ export class UsersController extends BaseController {
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const user = new User(req.body)
-    const newUser = await user.save()
-    res.status(201).send(newUser)
+      const newUser = await user.save()
+      res.status(201).send(newUser)
     } catch (error: any) {
       this.sendCreateUpdateErrorResponse(res, error)
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(401).send({ code: 401, error: 'user not found!' })
+    }
+
+    if (!await AuthService.comparePasswords(password, user.password)) {
+      return res.status(401).send({ code: 401, error: 'password does not match!' })
+    }
+
+    const token = AuthService.generateToken(user.toJSON())
+    return res.status(200).send({ token })
   }
 }
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,5 +1,6 @@
 import { User } from '@src/models/user';
 import { AuthService } from '@src/services/auth';
+import { faker } from '@faker-js/faker'
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
@@ -51,6 +52,47 @@ describe('Users functional tests', () => {
         code: 409,
         error: 'User validation failed: email: already exists in the database.'
       })
+    });
+  });
+
+  describe('When authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
+      const newUser = {
+        name: faker.name.fullName(),
+        email: faker.internet.email(),
+        password: faker.datatype.uuid()
+      }
+      await new User(newUser).save()
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: newUser.password})
+
+      expect(response.body).toEqual(expect.objectContaining({ token: expect.any(String) }))
+    });
+
+    it('should return UNAUTHORIZED if the user with the given email is not found', async () => {
+      const email = faker.internet.email()
+      const password = faker.datatype.uuid()
+
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email, password})
+
+      expect(response.status).toBe(401)
+    });
+
+    it('should return UNAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: faker.name.fullName(),
+        email: faker.internet.email(),
+        password: faker.datatype.uuid()
+      }
+      await new User(newUser).save()
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: "different_password"})
+
+      expect(response.status).toBe(401)
     });
   });
 });
